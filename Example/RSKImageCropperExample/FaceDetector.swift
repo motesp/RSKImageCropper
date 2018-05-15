3661e5b04572d68d129df7bfa98c5ec4f6a92277
Jonny Johansson <jonny.k.johansson@gmail.com>
2018-05-15 17:08:53 +0200

    Added basic face-detection to the example-app + changed so that you can select any image from the camera roll

---
+++ Example/RSKImageCropperExample/FaceDetector.swift
@@ -0,0 +1,78 @@
+//
+//  Test.swift
+//  RSKImageCropperExample
+//
+//  Created by Jonny Johansson on 2018-05-11.
+//  Copyright © 2018 Ruslan Skorb. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Vision
+
+@available(iOS 11.0, *)
+@objc class FaceDetector: NSObject {
+
+    var completionHandler: (CGRect)->()
+    var image: UIImage
+    
+    init(image:UIImage, faceDetectedHandler:@escaping (CGRect)->()) {
+        self.completionHandler = faceDetectedHandler
+        self.image = image
+
+        super.init()
+    }
+    
+    func detectFace() {
+        let orientation = CGImagePropertyOrientation(self.image.imageOrientation)
+        let faceLandmarksRequest = VNDetectFaceLandmarksRequest(completionHandler: self.handleFaceFeatures)
+        let requestHandler = VNImageRequestHandler(cgImage: self.image.cgImage!, orientation: orientation ,options: [:])
+        
+        do {
+            try requestHandler.perform([faceLandmarksRequest])
+        } catch {
+            print(error)
+        }
+    }
+
+    func handleFaceFeatures(request: VNRequest, errror: Error?) {
+        guard let observations = request.results as? [VNFaceObservation] else {
+            debugPrint("Face detection failed")
+            return
+        }
+        
+        debugPrint("Face detection complete! Found \(observations.count) face(s)")
+        
+        if let firstFace = observations.first {
+            let w = firstFace.boundingBox.size.width * image.size.width
+            let h = firstFace.boundingBox.size.height * image.size.height
+            let x = firstFace.boundingBox.origin.x * image.size.width
+            let y = firstFace.boundingBox.origin.y * image.size.height
+            
+            let margin: CGFloat = 1.0;
+            let firstFaceRect = CGRect(x: x-w*margin, y: image.size.height-y-h-h*margin, width: w+w*margin*2, height: h+h*margin*2)
+            
+            debugPrint("First face located at \(firstFaceRect.origin.x), \(firstFaceRect.origin.y), \(firstFaceRect.size.width), \(firstFaceRect.size.height)")
+
+            completionHandler(firstFaceRect)
+        } else {
+            completionHandler(.zero)
+        }
+    }
+    
+}
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImageOrientation) {
+        switch uiOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
--- Example/RSKImageCropperExample/RSKExampleViewController.m
+++ Example/RSKImageCropperExample/RSKExampleViewController.m
@@ -24,15 +24,21 @@
 
 #import "RSKExampleViewController.h"
 #import "RSKImageCropper.h"
+#import "RSKImageCropperExample-Swift.h"
 
 static const CGFloat kPhotoDiameter = 130.0f;
 static const CGFloat kPhotoFrameViewPadding = 2.0f;
 
-@interface RSKExampleViewController () <RSKImageCropViewControllerDelegate>
+@interface RSKExampleViewController () <RSKImageCropViewControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (strong, nonatomic) UIView *photoFrameView;
+@property (strong, nonatomic) UIView *photoFrameView2;
 @property (strong, nonatomic) UIButton *addPhotoButton;
+@property (strong, nonatomic) UIButton *addPhotoButton2;
 @property (assign, nonatomic) BOOL didSetupConstraints;
+@property (assign, nonatomic) CGRect zoomToRect;
+@property (assign, nonatomic) BOOL doZoom;
+@property (assign, nonatomic) BOOL imagePicked;
 
 @end
 
@@ -61,6 +67,13 @@ static const CGFloat kPhotoFrameViewPadding = 2.0f;
     self.photoFrameView.layer.cornerRadius = (kPhotoDiameter + kPhotoFrameViewPadding) / 2;
     [self.view addSubview:self.photoFrameView];
     
+    self.photoFrameView2 = [[UIView alloc] init];
+    self.photoFrameView2.backgroundColor = [UIColor colorWithRed:182/255.0f green:182/255.0f blue:187/255.0f alpha:1.0f];
+    self.photoFrameView2.translatesAutoresizingMaskIntoConstraints = NO;
+    self.photoFrameView2.layer.masksToBounds = YES;
+    self.photoFrameView2.layer.cornerRadius = (kPhotoDiameter + kPhotoFrameViewPadding) / 2;
+    [self.view addSubview:self.photoFrameView2];
+    
     // ---------------------------
     // Add the button "add photo".
     // ---------------------------
@@ -77,6 +90,19 @@ static const CGFloat kPhotoFrameViewPadding = 2.0f;
     [self.addPhotoButton setTitleColor:[UIColor colorWithRed:0/255.0f green:122/255.0f blue:255/255.0f alpha:1.0f] forState:UIControlStateNormal];
     [self.addPhotoButton addTarget:self action:@selector(onAddPhotoButtonTouch:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.addPhotoButton];
+    
+    self.addPhotoButton2 = [[UIButton alloc] init];
+    self.addPhotoButton2.backgroundColor = [UIColor whiteColor];
+    self.addPhotoButton2.translatesAutoresizingMaskIntoConstraints = NO;
+    self.addPhotoButton2.layer.masksToBounds = YES;
+    self.addPhotoButton2.layer.cornerRadius = kPhotoDiameter / 2;
+    self.addPhotoButton2.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.addPhotoButton2.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.addPhotoButton2.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [self.addPhotoButton2 setTitle:@"add\nphoto\n+\nzoom" forState:UIControlStateNormal];
+    [self.addPhotoButton2 setTitleColor:[UIColor colorWithRed:0/255.0f green:122/255.0f blue:255/255.0f alpha:1.0f] forState:UIControlStateNormal];
+    [self.addPhotoButton2 addTarget:self action:@selector(onAddPhotoButtonTouch2:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.addPhotoButton2];
     
     // ----------------
     // Add constraints.
@@ -97,48 +123,56 @@ static const CGFloat kPhotoFrameViewPadding = 2.0f;
     // The frame of the photo.
     // ---------------------------
     
-    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual
-                                                                     toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f
-                                                                   constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
+    NSLayoutConstraint *constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
     [self.photoFrameView addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual
-                                                 toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f
-                                               constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
     [self.photoFrameView addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual
-                                                 toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f
-                                               constant:0.0f];
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f];
     [self.view addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual
-                                                 toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0f
-                                               constant:0.0f];
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0f constant:0.0f];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView2 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
+    [self.photoFrameView2 addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView2 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:(kPhotoDiameter + kPhotoFrameViewPadding)];
+    [self.photoFrameView2 addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView2 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.photoFrameView2 attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0f constant:150.0f];
     [self.view addConstraint:constraint];
     
     // ---------------------------
     // The button "add photo".
     // ---------------------------
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual
-                                                 toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f
-                                               constant:kPhotoDiameter];
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:kPhotoDiameter];
     [self.addPhotoButton addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual
-                                                 toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f
-                                               constant:kPhotoDiameter];
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:kPhotoDiameter];
     [self.addPhotoButton addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual
-                                                 toItem:self.photoFrameView attribute:NSLayoutAttributeCenterX multiplier:1.0f
-                                               constant:0.0f];
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.photoFrameView attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f];
     [self.view addConstraint:constraint];
     
-    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual
-                                                 toItem:self.photoFrameView attribute:NSLayoutAttributeCenterY multiplier:1.0f
-                                               constant:0.0f];
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.photoFrameView attribute:NSLayoutAttributeCenterY multiplier:1.0f constant:0.0f];
+    [self.view addConstraint:constraint];
+ 
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton2 attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:kPhotoDiameter];
+    [self.addPhotoButton2 addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton2 attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0f constant:kPhotoDiameter];
+    [self.addPhotoButton2 addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton2 attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.photoFrameView attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f];
+    [self.view addConstraint:constraint];
+    
+    constraint = [NSLayoutConstraint constraintWithItem:self.addPhotoButton2 attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.photoFrameView attribute:NSLayoutAttributeCenterY multiplier:1.0f constant:150.0f];
     [self.view addConstraint:constraint];
     
     self.didSetupConstraints = YES;
@@ -146,24 +180,88 @@ static const CGFloat kPhotoFrameViewPadding = 2.0f;
 
 #pragma mark - Action handling
 
-- (void)onAddPhotoButtonTouch:(UIButton *)sender
-{
-    UIImage *photo = [UIImage imageNamed:@"photo"];
-    RSKImageCropViewController *imageCropVC = [[RSKImageCropViewController alloc] initWithImage:photo cropMode:RSKImageCropModeCircle];
+- (void)onAddPhotoButtonTouch:(UIButton *)sender {
+    self.zoomToRect = CGRectZero;
+    self.doZoom = false;
+    self.imagePicked = false;
+
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+    
+//    [self imagePickerController:nil didFinishPickingMediaWithInfo:nil];
+}
+
+- (void)onAddPhotoButtonTouch2:(UIButton *)sender {
+    self.zoomToRect = CGRectZero;
+    self.doZoom = true;
+    self.imagePicked = false;
+
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePickerController.delegate = self;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+    
+//    [self imagePickerController:nil didFinishPickingMediaWithInfo:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    if (self.imagePicked) {
+        return;
+    }
+    self.imagePicked = true;
+
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    //UIImage *image = [UIImage imageNamed:@"photo"];
+
+    if (@available(iOS 11.0, *)) {
+        if (self.doZoom) {
+            FaceDetector *fd = [[FaceDetector alloc] initWithImage:image faceDetectedHandler:^(CGRect rect) {
+                self.zoomToRect = rect;
+                
+                RSKImageCropViewController *imageCropVC = [[RSKImageCropViewController alloc] initWithImage:image cropMode:RSKImageCropModeCircle];
+                imageCropVC.delegate = self;
+                
+                [self.navigationController pushViewController:imageCropVC animated:YES];
+            }];
+            
+            [fd detectFace];
+            
+            return;
+        }
+    }
+
+    RSKImageCropViewController *imageCropVC = [[RSKImageCropViewController alloc] initWithImage:image cropMode:RSKImageCropModeCircle]; // RSKImageCropModeCustom
     imageCropVC.delegate = self;
+    
     [self.navigationController pushViewController:imageCropVC animated:YES];
 }
 
 #pragma mark - RSKImageCropViewControllerDelegate
 
-- (void)imageCropViewControllerDidCancelCrop:(RSKImageCropViewController *)controller
-{
+- (void)imageCropViewControllerDidDisplayImage:(RSKImageCropViewController *)controller {
+    if (!CGRectIsEmpty(self.zoomToRect)) {
+        NSLog(@"Zooming to %f, %f, %f, %f", self.zoomToRect.origin.x, self.zoomToRect.origin.y, self.zoomToRect.size.width, self.zoomToRect.size.height);
+
+        [controller zoomToRect:self.zoomToRect animated:YES];
+    }
+}
+
+- (void)imageCropViewControllerDidCancelCrop:(RSKImageCropViewController *)controller {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-- (void)imageCropViewController:(RSKImageCropViewController *)controller didCropImage:(UIImage *)croppedImage usingCropRect:(CGRect)cropRect rotationAngle:(CGFloat)rotationAngle
-{
-    [self.addPhotoButton setImage:croppedImage forState:UIControlStateNormal];
+- (void)imageCropViewController:(RSKImageCropViewController *)controller didCropImage:(UIImage *)croppedImage usingCropRect:(CGRect)cropRect rotationAngle:(CGFloat)rotationAngle {
+    if (self.doZoom) {
+        [self.addPhotoButton2 setImage:croppedImage forState:UIControlStateNormal];
+    } else {
+        [self.addPhotoButton setImage:croppedImage forState:UIControlStateNormal];
+    }
+    
     [self.navigationController popViewControllerAnimated:YES];
 }
 
